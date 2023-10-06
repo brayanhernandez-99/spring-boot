98335ae46910d87ab60bfbbd8da65f0468dd2fbe
brayanhernandez-99 <bhrestrepo_27@hotmail.com>
2023-10-05 19:32:34 -0500

    added table.sql

---
+++ src/main/resources/templates/database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id bigint primary key,
+    name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar(255) not null,
+    password varchar(50) not null,
+    phone varchar(20) not null,
+    age smallint(3) not null
+);
