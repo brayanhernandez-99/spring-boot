c5d26cca3c11cb8471c8ad9cbf621aa5fa4c7201
brayanhernandez-99 <bhrestrepo_27@hotmail.com>
2023-10-09 16:55:06 -0500

    updated models

--- src/main/java/com/springboot/example/models/Person.java
+++ src/main/java/com/springboot/example/models/Person.java
@@ -2,14 +2,21 @@ package com.springboot.example.models;
 
 import lombok.*;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+@Entity @Table(name = "users")
 public class Person {
 
+    @Column(name = "id")
     private long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "last_name")
     private String last_name;
-    private String email;
 
 }
--- src/main/java/com/springboot/example/models/User.java
+++ src/main/java/com/springboot/example/models/User.java
@@ -2,22 +2,35 @@ package com.springboot.example.models;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.Date;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+@Entity @Table(name = "users")
 public class User extends Person{
 
-    private String rol;
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "rol")
+    private String rol;
+
+    @Column(name = "createdAt")
     private Date createdAt;
+
+    @Column(name = "updateAt")
     private Date updateAt;
 
-    public User(long id, String name, String last_name, String email, String rol, String password, Date createdAt, Date updateAt) {
-        super(id, name, last_name, email);
-        this.rol = rol;
+    public User(long id, String name, String last_name, String email, String password, String rol, Date createdAt, Date updateAt) {
+        super(id, name, last_name);
+        this.email = email;
         this.password = password;
+        this.rol = rol;
         this.createdAt = createdAt;
         this.updateAt = updateAt;
     }
