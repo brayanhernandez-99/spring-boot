c415225f1230c79a7aa0f43d749dc1b5d3b19825
brayanhernandez-99 <bhrestrepo_27@hotmail.com>
2023-10-10 18:27:12 -0500

    created dao

--- src/main/java/com/springboot/example/dao/UserDAO.java
+++ src/main/java/com/springboot/example/dao/UserDAO.java
@@ -1,10 +1,20 @@
 package com.springboot.example.dao;
 
 import com.springboot.example.models.User;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface UserDAO {
+
+    User createUser(@RequestBody User user);
+
+    User updateUser(long id);
+
+    User delUser(long id);
+
+    User getUser(long id);
+
     List<User> getUsers();
 
 }
