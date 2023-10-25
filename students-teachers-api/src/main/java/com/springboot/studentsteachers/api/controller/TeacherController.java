c91cb758e938f8eec61421888807d1f26c03f6db
bhernandez <bhrestrepo_27@hotmail.com>
2023-10-25 09:04:59 -0500

    refactor project

---
+++ students-teachers-api/src/main/java/com/springboot/studentsteachers/api/controller/TeacherController.java
@@ -0,0 +1,43 @@
+package com.springboot.studentsteachers.api.controller;
+
+import com.springboot.studentsteachers.api.respository.StudentRepository;
+import com.springboot.studentsteachers.api.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class TeacherController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @PostMapping
+    public void createUser(@RequestBody Student student) {
+        studentRepository.createUser(student);
+    }
+
+    @PutMapping
+    public void updateUser(@RequestBody Student student) {
+        studentRepository.updateUser(student);
+    }
+
+    @DeleteMapping
+    public void deleteUser(@RequestHeader String email) {
+        studentRepository.deleteUser(email);
+    }
+
+    @GetMapping("/{email}")
+    public Student getUser(@PathVariable String email) {
+        //return userDao.getUser(email).stream().filter(user_dni -> user_dni.equals(email)).findFirst().orElseThrow(() -> new BadRequestException(String.format("User wit email [%s] not exist", email)));
+        return studentRepository.getUser(email);
+    }
+
+    @GetMapping
+    public List<Student> getUsers() {
+        return studentRepository.getUsers();
+    }
+
+}
