181a074b135c1e1e31f2876ae776b976673e42dc
bhernandez <bhrestrepo_27@hotmail.com>
2023-10-26 15:08:42 -0500

    updated exception

--- users-api/src/main/java/com/springboot/users/api/exception/UserBadRequestException.java
+++ users-api/src/main/java/com/springboot/users/api/exception/UserBadRequestException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class BadRequestException extends RuntimeException{
+public class UserBadRequestException extends RuntimeException{
 
-    public BadRequestException(String message){
+    public UserBadRequestException(String message){
         super(message);
     }
 
