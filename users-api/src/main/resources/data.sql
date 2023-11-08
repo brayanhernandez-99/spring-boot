7f989d6cbca108a367e51d9e583c00a6883324d6
bhernandez <bhrestrepo_27@hotmail.com>
2023-11-07 21:04:50 -0500

    updated files

--- users-api/src/main/resources/data.sql
+++ users-api/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (dni, name, last_name, email, password, rol, created_at, updated_at) VALUES
 (111, "Admin", "Admin", "admin@test.com", "pass123", "ADMIN", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(222, "Test1", "Test1", "test1@test.com", "pass123", "USER", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(333, "Test2", "Test2", "test2@test.com", "pass123", "USER", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(222, "User1", "User1", "user1@test.com", "pass123", "USER", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(333, "User2", "User2", "user2@test.com", "pass123", "USER", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
