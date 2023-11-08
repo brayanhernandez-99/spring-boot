a618f48f41ee2ffcee8c26857ad324e02d21b6fd
bhernandez <bhrestrepo_27@hotmail.com>
2023-11-07 20:54:19 -0500

    added schema.sql

---
+++ students-teachers-api/src/main/resources/schema.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE students_teachers;
+
+CREATE TABLE persons(
+    dni bigint not null,
+    name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar(100) not null,
+    password varchar(50) not null,
+    rol varchar(50) not null,
+    created_at datetime not null,
+    updated_at datetime not null,
+    primary key(dni)
+);
+
+CREATE TABLE subjects(
+    id int not null auto_increment,
+    name varchar(50) not null,
+    primary key(id)
+);
+
+CREATE TABLE students_subjects(
+    student bigint not null,
+    subject int not null,
+    foreign key(student) references persons(dni),
+    foreign key(subject) references subjects(id),
+    primary key(student, subject)
+);
+
+CREATE TABLE teachers_students(
+    teacher bigint not null,
+    student bigint not null,
+    foreign key(teacher) references persons(dni),
+    foreign key(student) references persons(dni),
+    primary key(teacher, student)
+);
+
